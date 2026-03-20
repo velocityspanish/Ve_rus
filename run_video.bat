90f16a49e8f4c5a201e585a83382fad1f2534758
velocityspanish <velocityspanish@gmail.com>
2026-03-20 18:24:37 +0530

    Add files via upload

---
+++ run_video.bat
@@ -0,0 +1,5 @@
+@echo off
+set PYTHONHOME=
+set PYTHONPATH=
+cd /d "C:\Users\kreg9\AppData\Local\Programs\Python\Python311"
+python.exe -s -E "C:\Users\kreg9\Downloads\kreggscode\qwen\bots\Youtube_bots\velocity Russian\facebook_reels_automation.py"
